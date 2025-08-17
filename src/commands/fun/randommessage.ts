90315822ef88af3ece9cd12a185e9156559cae3e
Henry Sanger <henry@sanger.io>
2025-08-17 01:27:18 +0000

    wip: random message command

--- src/commands/fun/randommessage.ts
+++ src/commands/fun/randommessage.ts
@@ -1,6 +1,8 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { type ChatInputCommandInteraction, Collection, type Message, SlashCommandBuilder, type Snowflake } from "discord.js";
 import { Command } from "../../utils/command";
-import { pickRandomInArray } from "../../utils/misc";
+import { createMessageLink } from "../../utils/misc";
+
+let lastId: Snowflake | undefined;
 
 export default new Command({
     data: new SlashCommandBuilder()
@@ -13,8 +15,17 @@ export default new Command({
 
         if (!interaction.channel) throw new Error("Unknown channel");
 
-        const messages = await interaction.channel.messages.fetch({ limit: 100 });
-        const message = pickRandomInArray(messages.values().toArray());
-        await interaction.followUp({ content: message.content, files: message.attachments.values().toArray(), embeds: message.embeds });
+        const getMessages = async (iterations: number, existing = new Collection<string, Message<boolean>>(), before?: Snowflake): Promise<Collection<string, Message<boolean>>> => {
+            if (!iterations || !interaction.channel) return existing;
+            const messages = await interaction.channel.messages.fetch({ limit: 100, before });
+            return messages.concat(await getMessages(--iterations, existing, messages.at(0)?.id));
+        };
+
+        const message = (await getMessages(10, undefined, lastId)).random();
+        if (message) {
+            await interaction.followUp({ content: message.content || "_ _", files: message.attachments.values().toArray(), embeds: message.embeds });
+        } else {
+            await interaction.followUp({ content: "Unable to fetch message" });
+        }
     }
 });
