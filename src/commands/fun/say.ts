72bc09dbe3451b07a3fce6c6a659316f6bae5c02
Henry Sanger <henry@sanger.io>
2025-08-20 21:17:06 +0000

    feat: allow replying to messages with /say; add logging

--- src/commands/fun/say.ts
+++ src/commands/fun/say.ts
@@ -1,5 +1,6 @@
-import { type ChatInputCommandInteraction, PermissionsBitField, SlashCommandBuilder } from "discord.js";
+import { type ChatInputCommandInteraction, Colors, EmbedBuilder, type Message, MessageFlags, PermissionsBitField, SlashCommandBuilder } from "discord.js";
 import { Command } from "../../utils/command";
+import { createMessageLink, getModLogChannel } from "../../utils/misc";
 
 export default new Command({
     data: new SlashCommandBuilder()
@@ -8,11 +9,12 @@ export default new Command({
         .addStringOption(option => option
             .setName("message")
             .setDescription("What to say")
+            .setMaxLength(2000)
             .setRequired(true)
         )
-        .addAttachmentOption(option => option
-            .setName("attachment")
-            .setDescription("Add an attachment to the message")
+        .addStringOption(option => option
+            .setName("reply_to")
+            .setDescription("The ID of the message to reply to (optional)")
         )
         .setDefaultMemberPermissions(PermissionsBitField.Flags.ManageMessages),
     cooldown: 1000,
@@ -20,9 +22,25 @@ export default new Command({
         const channel = interaction.channel;
         if (!channel?.isSendable()) return;
 
-        await channel.send({ content: interaction.options.getString("message", true) });
+        const content = interaction.options.getString("message", true);
+        let message: Message;
 
-        await interaction.reply({ content: "Message sent." });
+        const replyToId = interaction.options.getString("reply_to");
+        if (replyToId) {
+            const replyTo = await channel.messages.fetch(replyToId);
+            message = await replyTo.reply({ content });
+        } else {
+            message = await channel.send({ content });
+        }
+
+        await interaction.reply({ content: "Message sent.", flags: [MessageFlags.Ephemeral] });
         await interaction.deleteReply();
+
+        const embed = new EmbedBuilder()
+            .setDescription(`### 🗣️ \`/say\` command used in <#${interaction.channelId}> by <@${interaction.user.id}>\n${createMessageLink(message)}\n${content}`)
+            .setColor(Colors.DarkVividPink)
+            .setTimestamp();
+        const logChannel = await getModLogChannel(interaction.guild);
+        logChannel.send({ embeds: [embed] });
     }
 });
